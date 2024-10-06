aef23c6e18fa7dffaf943b48d8d52a0a8f11ff6f
Alberto Carlos <albertocarlos.carbonell@weecover.com>
2024-10-06 14:14:34 -0300

    check if style fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://accarbonell1987.github.io",
-  base: "rs-landing-page",
 });
